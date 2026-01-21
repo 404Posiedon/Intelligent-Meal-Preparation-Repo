504a78eeb56898f00a24e5876b1f7fef3e6580a5
Khodani <ludere.khodani@outlook.com>
2026-01-21 23:53:02 +0200

    (Add): Inital ApplicationDbContext setup and models registrations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IntelligentMealPreparationAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntelligentMealPreparationAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Register DbSets for the models
+        public DbSet<User> Users { get; set; }
+        public DbSet<Preference> Preferences { get; set; }
+        public DbSet<DietaryRestriction> DietaryRestrictions { get; set; }
+        public DbSet<Allergies> Allergies { get; set; }
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+        public DbSet<MealPlan> MealPlans { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<PantryItem> PantryItems { get; set; }
+        public DbSet<MealIngredient> MealIngredients { get; set; }
+        public DbSet<ShoppingListItem> ShoppingListItems { get; set; }
+
+
+    }
+}
